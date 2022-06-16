b72f4abd863b3a3374b8983b9671c20c30566c95
Marisol <hcmarisol@uaem.mx>
2022-06-16 18:10:45 -0500

    Updates

---
+++ precache-manifest.225259d0305e335b6693088967f3af4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03a90ca07c4830933f11",
+    "url": "/pizzasmar-Hernandez/static/css/main.2187496d.chunk.css"
+  },
+  {
+    "revision": "03a90ca07c4830933f11",
+    "url": "/pizzasmar-Hernandez/static/js/main.03a90ca0.chunk.js"
+  },
+  {
+    "revision": "53901bb39c9d5db2bdc2",
+    "url": "/pizzasmar-Hernandez/static/css/1.3ca02894.chunk.css"
+  },
+  {
+    "revision": "53901bb39c9d5db2bdc2",
+    "url": "/pizzasmar-Hernandez/static/js/1.53901bb3.chunk.js"
+  },
+  {
+    "revision": "e98f19592b90efde494e",
+    "url": "/pizzasmar-Hernandez/static/js/runtime~main.e98f1959.js"
+  },
+  {
+    "revision": "81a83ae116cfd1529a2f3ecfe36affb6",
+    "url": "/pizzasmar-Hernandez/index.html"
+  }
+];
